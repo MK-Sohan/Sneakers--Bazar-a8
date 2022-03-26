16cf45f7b3f814dfb8d75f3a38e8ef4f94a5118c
MK-Sohan <mksohan786@gmail.com>
2022-03-26 13:45:45 +0600

    third

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,11 +5,12 @@ const Cart = (props) => {
     const {cart}=props
     console.log(cart);
     // console.log(cart);
- 
+
     return (
         <div>
+           
             {
-                cart.map(item=><h1>Name:{item.name}</h1>)
+                cart.map(item=><h3>Name:{item.name}</h3>)
             }
             
             <button className='btn1'>Choose One For Me</button> <br />
